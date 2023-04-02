a155980afbe3ea82dfde2637f14230bd349253a0
claudiovitor2001 <claudiovitor2001@gmail.com>
2023-04-02 00:16:36 -0300

    renderizar listas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Teste from './component/Teste';
 import Btn from './component/Btn';
 import Formulario from './component/Formulario';
 import Email from './component/Email';
+import Lista from './component/Lista';
 
 function App() {
+  const meusItens = ['Comida', 'Bebida', 'Sobremesa']
+
   return (
     <div className="App">
       <header className="App-header">
         <Btn/>
+        <Lista itens={meusItens}></Lista>
+        <Lista itens={[]}/>
         <h1>Pessoas:</h1>
         <p>
           <Teste name="João" age="18" hobby="Violão"/>
---
+++ src/component/Lista.js
@@ -0,0 +1,17 @@
+function Lista({ itens }){
+    return(
+        <>
+            <h3>Lista de coisas boas:</h3>
+            {itens.length> 0?(
+                itens.map((item, index)=>(
+                    <p key={index}>{item}</p>
+                ))
+            ):(
+                <p>Não há itens nessa lista.</p>
+            )}
+            
+        </>
+    )
+}
+
+export default Lista
